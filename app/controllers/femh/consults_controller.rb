707a770ae7047a8b6cbd653322c50717b4e531bb
Daniel K. Lin <yettie.lin@gmail.com>
2016-01-24 05:49:52 +0000

    fix nav bar to show 2016 month link

--- app/controllers/femh/consults_controller.rb
+++ app/controllers/femh/consults_controller.rb
@@ -14,9 +14,19 @@ class Femh::ConsultsController < AdminController
     def show
         
         @month_string = params[:id].to_s
-        @days = Time.days_in_month(@month_string.to_i,2015).to_s
-        @month_start = "2015-"+"#{@month_string}"+"-01" 
-        @month_end = "2015-"+"#{@month_string}"+"-"+"#{@days}"
+        
+        year = @month_string[0,4]
+        month = @month_string[4,2]
+        
+        #@days = Time.days_in_month(@month_string.to_i,2015).to_s
+        #@month_start = "2015-"+"#{@month_string}"+"-01" 
+        #@month_end = "2015-"+"#{@month_string}"+"-"+"#{@days}"
+
+        @days = Time.days_in_month(month.to_i,year).to_s
+        @month_start = "#{year}-"+"#{month}"+"-01" 
+        @month_end = "#{year}-"+"#{month}"+"-"+"#{@days}"
+
+
         
      @consult_all_month = Consult.where(check_date1: "#{@month_start}".."#{@month_end}").group("check_type").order("check_type").group("doc_name").order("sum_patient_no_only DESC").sum("patient_no_only")
      
--- lookFEMG-Nov-C9.rb
+++ lookFEMG-Nov-C9.rb
@@ -104,8 +104,8 @@ end
 		fm_room = ["F107", "F108", "F109", "F110", "F111", "2012"]
 		
 		
-		year_month = "10411"
-	  	month_day = ["26"]		
+		year_month = "10501"
+	  	month_day = ["05"]		
 	#	year_month = "10410"
 	#	month_day = ["17"]
 	
